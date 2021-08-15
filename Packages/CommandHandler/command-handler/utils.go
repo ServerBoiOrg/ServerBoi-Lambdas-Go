986ce7fb465a21f5e8c13a4fac32758affa62990
awlsring <mattcanemail@gmail.com>
2021-08-15 14:45:00 -0700

    adding more aws regions to allowlist

--- Packages/CommandHandler/command-handler/utils.go
+++ Packages/CommandHandler/command-handler/utils.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	awsRegions       = []string{"us-west-2"}
+	awsRegions       = []string{"us-east-1, us-east-2, us-west-1, us-west-2, eu-west-1, eu-west-2, eu-west-3, eu-north-1"}
 	serverboiRegions = []string{"us-west"}
 )
 
